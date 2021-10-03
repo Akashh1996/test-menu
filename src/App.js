e4f247cf8496b6b1a1b8bce008c7b6352cc3126e
Akash Sapkota <akashh.sapkota@gmail.com>
2021-10-03 17:30:33 +0200

    test-appç

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
+import {useState, useEffect, useRef} from "react"
 import './App.css';
 
+
+const menu = ["Cycle", "Running", "Outdoor", "Swim", "triathlon", "GYM", "sportswear", "Nutrition"]
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+  const handleClick = () => !isOpen ? setIsOpen(true) : setIsOpen(false)
+  const [startingX, setstartingX] = useState(0);
+  const [movingX, setmovingX] = useState(0);
+  const [startingY, setstartingY] = useState(0);
+
+  const [movingY, setmovingY] = useState(0);
+  const [isTouchLeft, setisTouchLeft] = useState(false) 
+
+  const touchStart = (e) => {
+    setstartingX(e.touches[0].clientX);
+    setstartingY(e.touches[0].clientY);
+  }
+
+  const touchMove = (e) => {
+    setmovingX(e.touches[0].clientX) ;
+    setmovingY(e.touches[0].clientY) ;
+    console.log(movingY)
+  }
+
+  const touchEnd = (e) => {
+ if(startingX-100 > movingX && movingY > 100 && movingX > 250){
+     setisTouchLeft(true)
+     setIsOpen(false)
+  }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App" className="main" onTouchStart={touchStart} onTouchMove={touchMove} onTouchEnd={touchEnd}>
+      {!isOpen && <button onClick={handleClick} className="open"><span>OpenMenu</span></button>}
+      {
+      <div className={isOpen ? "menu-content animate" : "menu-content"}>
+      <button onClick={handleClick} className="close"><span>X</span></button>
+      <ul>
+      {menu.map((m, i)=> <li key={i}>{m}</li>)}
+      </ul>
+      </div>
+      }
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Content
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
